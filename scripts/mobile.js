92492f9625a83169b84be4f53862b69a50e16da8
kshitizrohilla <kshitiz13@gmail.com>
2023-05-20 23:11:44 +0530

    Made the website mobile-friendly
    Major changes to the top bar and removed both the additional rows for the mobile. Made a separate javascript file for mobile fetch.

---
+++ scripts/mobile.js
@@ -0,0 +1,95 @@
+var cityInputMobile = document.getElementById("mobileSearchCity");
+
+cityInputMobile.addEventListener("keyup", function(event)
+{
+  if(event.key === "Enter")
+  {
+		loader();
+		function loader()
+		{
+
+			document.getElementById("locationName").innerHTML = "";
+			document.getElementById("temperatureValue").innerHTML = "";
+			document.getElementById("weatherType").innerHTML = "";
+
+			const img1 = document.createElement("img");
+			const img2 = document.createElement("img");
+			const img3 = document.createElement("img");
+
+			img1.id = "loader1";
+			img2.id = "loader2";
+			img3.id = "loader3";
+
+			img1.src = "icons/loader.gif";
+			img2.src = "icons/loader.gif";
+			img3.src = "icons/loader.gif";
+
+			const parentElement1 = document.getElementById("locationName");
+			const parentElement2 = document.getElementById("temperatureValue");
+			const parentElement3 = document.getElementById("weatherType");
+
+			parentElement1.appendChild(img1);
+			parentElement2.appendChild(img2);
+			parentElement3.appendChild(img3);
+
+			// document.getElementById("loader1").src = "icons/loader.gif";
+			// document.getElementById("loader2").src = "icons/loader.gif";
+			// document.getElementById("loader3").src = "icons/loader.gif";
+		}
+
+    var cityInputValue = cityInputMobile.value;
+
+    var apiKey = "b1fd6e14799699504191b6bdbcadfc35"; // Default
+    var unit = "metric";
+    var apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityInputValue}&appid=${apiKey}&units=${unit}`;
+
+    if(cityInputValue != "")
+    {
+      async function getWeather()
+      {
+        var response = await fetch(apiUrl);
+        var data = await response.json();
+
+        if(data.message != "city not found" && data.cod != "404")
+        {
+          var location = data.name;
+          var temperature = data.main.temp;
+          var weatherType = data.weather[0].description;
+          var realFeel = data.main.feels_like;
+          var windSpeed = data.wind.speed;
+          var windDirection = data.wind.deg;
+          var visibility = data.visibility / 1000;
+          var pressure = data.main.pressure;
+          var maxTemperature = data.main.temp_max;
+          var minTemperature = data.main.temp_min;
+          var humidity = data.main.humidity;
+          var sunrise = data.sys.sunrise;
+          var sunset = data.sys.sunset;
+        
+          document.getElementById("locationName").innerHTML = location;
+          document.getElementById("temperatureValue").innerHTML = temperature + "<sup>o</sup>C";
+          document.getElementById("weatherType").innerHTML = weatherType;
+          document.getElementById("realFeelAdditionalValue").innerHTML = realFeel + "<sup>o</sup>C";
+          document.getElementById("windSpeedAdditionalValue").innerHTML = windSpeed + " km/h";
+          document.getElementById("windDirectionAdditionalValue").innerHTML = windDirection;
+          document.getElementById("visibilityAdditionalValue").innerHTML = visibility + " km";
+          document.getElementById("pressureAdditionalValue").innerHTML = pressure;
+          document.getElementById("maxTemperatureAdditionalValue").innerHTML = maxTemperature + "<sup>o</sup>C";
+          document.getElementById("minTemperatureAdditionalValue").innerHTML = minTemperature + "<sup>o</sup>C";
+          document.getElementById("humidityAdditionalValue").innerHTML = humidity;
+          document.getElementById("sunriseAdditionalValue").innerHTML = sunrise;
+          document.getElementById("sunsetAdditionalValue").innerHTML = sunset;
+        }
+        else
+				{
+					document.getElementById("locationName").innerHTML = "City Not Found";
+					document.getElementById("temperatureValue").innerHTML = "";
+					document.getElementById("weatherType").innerHTML = "";
+				}
+      }
+
+      getWeather();
+    }
+    else document.getElementById("locationName").innerHTML = "Enter a city name...";
+  }
+});
